7db61220509bc6029dc199c1f49e9823f4ac24cb
anhtuan <huynhanhtuan266@gmail.com>
2023-02-17 14:11:00 +0700

    set up ejs

--- app.js
+++ app.js
@@ -1,19 +1,25 @@
-const express = require("express");
+const express = require('express');
+const path = require('path');
 const app = express();
-
-app.set("views", "./views");
-app.set("view engine", "ejs");
+app.set('view engine', 'ejs');
 // static files
-app.use(express.static("public"));
-app.use("/css", express.static(__dirname + "public/css"));
-app.use("/js", express.static(__dirname + "public/js"));
-app.use("/img", express.static(__dirname + "public/img"));
-app.use("views", express.static(__dirname + "views"));
+
+app.use('/', express.static(path.join(__dirname, '/public')));
+
+//localhost:3000/demo/user.html
+//localhost:3000/demo/user
+http: app.use('/demo', require('./routes/test'));
+
+// app.use(express.static("public"));
+// app.use("/css", express.static(__dirname + "public/css"));
+// app.use("/js", express.static(__dirname + "public/js"));
+// app.use("/img", express.static(__dirname + "public/img"));
+// app.use("views", express.static(__dirname + "views"));
 const port = 3000;
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
 
-app.get("/", (req, res) => {
-  res.render("index");
+app.get('/', (req, res) => {
+  res.render('index');
 });

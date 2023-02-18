d2e1b8751562703cc6b7b28c480c11bc952aebb3
ViTrans <tran.vi487@gmail.com>
2023-02-18 23:45:46 +0700

    render giao dien va trang chi tiet

--- app.js
+++ app.js
@@ -1,14 +1,14 @@
 // Imports
-const express = require('express');
+const express = require("express");
 const app = express();
 const port = 5000;
-require('dotenv').config();
-const mongoose = require('mongoose');
-const Post = require('./src/models/posts');
+require("dotenv").config();
+const mongoose = require("mongoose");
+const Post = require("./src/models/posts");
 
 // conect DB
 // Connection URL. This is where your mongodb server is running.
-mongoose.set('strictQuery', true);
+mongoose.set("strictQuery", true);
 const conectDB = async () => {
   try {
     await mongoose.connect(process.env.DB_URI, {
@@ -20,33 +20,44 @@ const conectDB = async () => {
   }
 };
 conectDB();
-mongoose.connection.once('open', () => {
-  console.log('connection open');
+mongoose.connection.once("open", () => {
+  console.log("connection open");
 });
 
 // Static Files
-app.use(express.static('public'));
+app.use(express.static("public"));
 // Specific folder example
 // app.use("/css", express.static(__dirname + "public/css"));
 // app.use("/js", express.static(__dirname + "public/js"));
 // app.use("/img", express.static(__dirname + "public/img"));
 
 // Set View's
-app.set('views', './src/views');
-app.set('view engine', 'ejs');
+app.set("views", "./src/views");
+app.set("view engine", "ejs");
 
 // Navigation
-app.get('', (req, res) => {
-  res.render('index', { title: 'Trang Chủ' });
+app.get("", async (req, res) => {
+  const posts = await Post.find();
+  res.render("index", { title: "Trang Chủ", posts });
 });
 
-app.get('/details', async (req, res) => {
-  const post = new Post({
-    title: ' NHÀ TRỌ SẠCH SẼ - CHO THUÊ PHÒNG TRỌ GẦN BẾN XE MIỀN ĐÔNG CŨ',
-    description: '82/12 Nguyễn Xí, Phường 26, Quận Bình Thạnh, Hồ Chí Minh',
-  });
+app.get("/details/:id", async (req, res) => {
+  const post = await Post.findById(req.params.id);
+  // const post = new Post({
+  //   title: "Nhà trọ 55/4, Trần Việt Châu",
+  //   description: "Nhà trọ thoáng mát, sạch sẽ, có chỗ để xe",
+  //   address: "82/12 Nguyễn Xí, Phường 26, Quận Bình Thạnh, Hồ Chí Minh",
+  //   price: 2000000,
+  //   phone: "0123456789",
+  //   createAt: Date.now(),
+  // });
+  // try {
+  //   await post.save();
+  // } catch (error) {
+  //   console.log(error);
+  // }
 
-  res.render('postDetails', { title: 'Chi Tiết', post });
+  res.render("postDetails", { title: "Chi Tiết", post });
 });
 
 app.listen(port, () => console.info(`App listening on port ${port}`));

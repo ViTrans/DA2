0b1ad62a5ec39b22d85704ddb75b927f9beb8155
anhtuan <huynhanhtuan266@gmail.com>
2023-02-18 21:36:03 +0700

    connect db

--- src/routes/test.js
+++ src/routes/test.js
@@ -3,6 +3,5 @@ const express = require('express');
 const router = express.Router();
 const testController = require('../controllers/test');
 
-// user or user.html deu match route
 router.get('/user(.html)?', testController.test);
 module.exports = router;

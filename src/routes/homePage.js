5641c758f579e6254a5402fe31d4b3dd33ddcfbb
ViTrans <tran.vi487@gmail.com>
2023-02-21 09:11:30 +0700

    cau hinh lai mvc

---
+++ src/routes/homePage.js
@@ -0,0 +1,7 @@
+const express = require("express");
+const homePageController = require("../controller/homePageController");
+const router = express.Router();
+
+router.get("/", homePageController.showHomePage);
+router.get("/details/:id", homePageController.showPostDetails);
+module.exports = router;

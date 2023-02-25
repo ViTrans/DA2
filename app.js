f709eda5488f08b2ba45961651dc843f4e0ae114
anhtuan <huynhanhtuan266@gmail.com>
2023-02-25 20:36:35 +0700

    My message

--- app.js
+++ app.js
@@ -1,63 +1,64 @@
 // Imports
-const express = require("express");
+const express = require('express');
 const app = express();
 const port = 5000;
-require("dotenv").config();
-const mongoose = require("mongoose");
-const Post = require("./src/models/posts");
+require('dotenv').config();
+const mongoose = require('mongoose');
+const Post = require('./src/models/posts');
 
 // conect DB
 // Connection URL. This is where your mongodb server is running.
-mongoose.set("strictQuery", true);
+mongoose.set('strictQuery', true);
 const conectDB = async () => {
   try {
     await mongoose.connect(process.env.DB_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+      dbName: 'doan2',
     });
   } catch (error) {
     console.log(error);
   }
 };
 conectDB();
-mongoose.connection.once("open", () => {
-  console.log("connection open");
+mongoose.connection.once('open', () => {
+  console.log('connection open');
 });
 
 // Static Files
-app.use(express.static("public"));
-// Specific folder example
-// app.use("/css", express.static(__dirname + "public/css"));
-// app.use("/js", express.static(__dirname + "public/js"));
-// app.use("/img", express.static(__dirname + "public/img"));
+app.use(express.static('public'));
 
 // Set View's
-app.set("views", "./src/views");
-app.set("view engine", "ejs");
+app.set('views', './src/views');
+app.set('view engine', 'ejs');
 
 // Navigation
-app.get("", async (req, res) => {
+app.get('', async (req, res) => {
+  const post = new Post({
+    title: 'Nhà trọ 109/23, Trần Duy Hung',
+    description: 'Nhà trọ thoáng mát, sạch sẽ, có chỗ để xe,hồ bơi',
+    address: '82/12 Nguyễn Xí, Phường 26, Quận Bình Thạnh, Hồ Chí Minh',
+    price: 300000,
+    phone: '0334133252',
+    createAt: Date.now(),
+  });
+  await post.save();
   const posts = await Post.find();
-  res.render("index", { title: "Trang Chủ", posts });
+
+  res.render('index', { title: 'Trang Chủ', posts });
 });
 
-app.get("/details/:id", async (req, res) => {
+app.get('/details/:id', async (req, res) => {
+  console.log(req.params);
   const post = await Post.findById(req.params.id);
-  // const post = new Post({
-  //   title: "Nhà trọ 55/4, Trần Việt Châu",
-  //   description: "Nhà trọ thoáng mát, sạch sẽ, có chỗ để xe",
-  //   address: "82/12 Nguyễn Xí, Phường 26, Quận Bình Thạnh, Hồ Chí Minh",
-  //   price: 2000000,
-  //   phone: "0123456789",
-  //   createAt: Date.now(),
-  // });
-  // try {
-  //   await post.save();
-  // } catch (error) {
-  //   console.log(error);
-  // }
 
-  res.render("postDetails", { title: "Chi Tiết", post });
+  try {
+    await post.save();
+  } catch (error) {
+    console.log(error);
+  }
+
+  res.render('postDetails', { title: 'Chi Tiết', post });
 });
 
 app.listen(port, () => console.info(`App listening on port ${port}`));

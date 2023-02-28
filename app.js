5b59642a4d4ebd92ff92ff67a004ad2ee64c985d
anhtuan <huynhanhtuan266@gmail.com>
2023-02-28 08:29:00 +0700

    "check"

--- app.js
+++ app.js
@@ -2,41 +2,29 @@
 const express = require('express');
 const app = express();
 const port = 5000;
-require("dotenv").config();
-const mongoose = require("mongoose");
+require('dotenv').config();
+const mongoose = require('mongoose');
+const createError = require('http-errors');
+const homePageRouter = require('./src/routes/homePage');
+const signupRouter = require('./src/routes/signupRouter');
+const signinRouter = require('./src/routes/signinRouter');
+const session = require('express-session');
+const flash = require('connect-flash');
+const Post = require('./src/models/posts');
 
-const homePageRouter = require("./src/routes/homePage");
-const signupRouter = require("./src/routes/signupRouter");
-const signinRouter = require("./src/routes/signinRouter");
-const session = require("express-session");
-const flash = require("connect-flash");
-const Post = require("./src/models/posts");
+// router
 
+const postRouter = require('./src/routes/post');
 
-// conect DB
-// Connection URL. This is where your mongodb server is running.
-mongoose.set('strictQuery', true);
-const conectDB = async () => {
-  try {
-    await mongoose.connect(process.env.DB_URI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
-  } catch (error) {
-    console.log(error);
-  }
-};
-conectDB();
-mongoose.connection.once('open', () => {
-  console.log('connection open');
-});
+// connect DB
+require('./src/helpers/connectDB');
 
 // Middleware
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(
   session({
-    secret: "secret key",
+    secret: 'secret key',
     resave: false,
     saveUninitialized: true,
   })
@@ -45,6 +33,7 @@ app.use(flash());
 
 const getUser = (req, res, next) => {
   res.locals.user = req.session.user;
+  console.log(req.session);
   next();
 };
 app.use(getUser);
@@ -55,15 +44,30 @@ app.use(express.static('public'));
 // Set View's
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
+app.get('/demo', (req, res) => {
+  res.status(200);
+  res.json({ msg: 'ahhih' });
+});
 
+// routers
 
-app.use("/", homePageRouter);
-app.use("/", homePageRouter);
-app.use("/", signupRouter);
-app.use("/", signinRouter);
-=======
+app.use('/', homePageRouter);
+app.use('/', homePageRouter);
+app.use('/', signupRouter);
+app.use('/', signinRouter);
 
+app.use('/quan-ly/posts', postRouter);
 
+// Middleware handle errors
+app.use((req, res, next) => {
+  next(createError.NotFound('đường dẫn truy cập máy chủ không hợp lệ'));
+});
+
+app.use((err, req, res, next) => {
+  res.json({
+    code: err.status || 500,
+    msg: err.message,
+  });
 });
 
 app.listen(port, () => console.info(`App listening on port ${port}`));

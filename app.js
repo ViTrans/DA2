f14df06f6e077ca3adec5a8046a5e9f5030aa6db
anhtuan <huynhanhtuan266@gmail.com>
2023-03-02 12:34:29 +0700

    up lai nha

--- app.js
+++ app.js
@@ -3,21 +3,38 @@ const express = require("express");
 const app = express();
 const port = 5000;
 require('dotenv').config();
-const mongoose = require('mongoose');
 const createError = require('http-errors');
+
+const mongoose = require('mongoose');
 const homePageRouter = require('./src/routes/homePage');
 const signupRouter = require('./src/routes/signupRouter');
 const signinRouter = require('./src/routes/signinRouter');
 const session = require('express-session');
 const flash = require('connect-flash');
-const Post = require('./src/models/posts');
-
-// router
+const moment = require('moment');
 
 const postRouter = require('./src/routes/post');
+// const categoryRouter = require('./src/routes/category');
 
-// connect DB
-require('./src/helpers/connectDB');
+// conect DB
+// Connection URL. This is where your mongodb server is running.
+mongoose.set('strictQuery', true);
+const conectDB = async () => {
+  try {
+    await mongoose.connect(process.env.DB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      dbName: 'doan2',
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
+conectDB();
+
+mongoose.connection.once('open', () => {
+  console.log('connection open');
+});
 
 // Middleware
 app.use((req, res, next) => {
@@ -47,19 +64,13 @@ app.use(express.static("public"));
 // Set View's
 app.set('views', './src/views');
 app.set('view engine', 'ejs');
-app.get('/demo', (req, res) => {
-  res.status(200);
-  res.json({ msg: 'ahhih' });
-});
-
-// routers
 
 app.use('/', homePageRouter);
 app.use('/', homePageRouter);
 app.use('/', signupRouter);
 app.use('/', signinRouter);
-
 app.use('/quan-ly/posts', postRouter);
+// app.use('/quan-ly/categories', categoryRouter);
 
 // Middleware handle errors
 app.use((req, res, next) => {

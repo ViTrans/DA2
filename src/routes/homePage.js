e1d9e0dd9276bf0e4b944650086a43d0ad1781b4
ViTrans <tran.vi487@gmail.com>
2023-04-03 17:17:46 +0700

    search and pagination

--- src/routes/homePage.js
+++ src/routes/homePage.js
@@ -4,5 +4,6 @@ const router = express.Router();
 
 router.get("/", homePageController.showHomePage);
 router.get("/category/:category_id", homePageController.getPostsByCategory);
+router.get("/search", homePageController.searchPost);
 
 module.exports = router;
